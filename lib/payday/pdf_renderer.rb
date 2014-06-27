14718e9579a51e47cc4231811729f7757de3dec4
Spencer Elliott <me@elliottsj.com>
2014-06-26 22:08:15 -0400

    Shipping rate

--- lib/payday/pdf_renderer.rb
+++ lib/payday/pdf_renderer.rb
@@ -211,7 +211,7 @@ module Payday
               invoice.tax_description.nil? ? I18n.t('payday.invoice.tax', :default => "Tax:") : invoice.tax_description),
               cell(pdf, number_to_currency(invoice.tax, invoice), :align => :right)]
         end
-        if invoice.shipping_rate > 0
+        if invoice.shipping > 0
           table_data << [bold_cell(pdf,
               invoice.shipping_description.nil? ? I18n.t('payday.invoice.shipping', :default => "Shipping:") : invoice.shipping_description),
               cell(pdf, number_to_currency(invoice.shipping, invoice), :align => :right)]
